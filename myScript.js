9728b625d78fcc6b186080af60b2d3d13bbf8019
EdnaStella <ednastella@hotmail.com>
2018-03-04 19:07:42 +0000

    origens

--- myScript.js
+++ myScript.js
@@ -59,3 +59,10 @@
 
 
 			}		
+			function show_div(id1, id2) {
+			// body...
+				objSelected=false;
+				document.getElementById(id1).style.display='none';
+				document.getElementById(id2).style.display='block';
+							
+}
